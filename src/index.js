02cc430c6aa14df1623704d5c4cc62d42ffbf158
Ameer <ameerapex496@gmail.com>
2023-11-13 13:10:27 +0100

    finished some logics

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
-console.log("start of project");
+// getting player elements
+const playerBtnWrapper = document.querySelectorAll(".player-Btn-Wrapper")[0];
+
+// getting pc elements
+const PcGameBtnWrapper = document.querySelectorAll(".Pc-game-Btn-Wrapper")[0];
+
+// our game func
+function randNum(e) {
+  // player functions
+  const playerScore = e.target.id;
+
+  playerBtnWrapper.children[playerScore].classList.add("active");
+  console.log("Player Point : ", playerScore);
+
+  // pc functions
+  const randNum = Math.round(Math.random() * 2);
+  console.log(PcGameBtnWrapper.children[randNum]);
+  PcGameBtnWrapper.children[randNum].classList.add("active");
+
+  // game functions
+  console.log("PC Point : ", randNum);
+  if (playerScore > randNum) {
+    console.log("payer won");
+  } else if (playerScore < randNum) {
+    console.log("payer lost");
+  } else {
+    console.log("same");
+  }
+}
+
+// our eventlinsters
+playerBtnWrapper.addEventListener("click", randNum);
