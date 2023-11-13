aa517429334d7d6d7264ee566fb57f32ac2e0b29
Ameer <ameerapex496@gmail.com>
2023-11-13 13:59:14 +0100

    new futures / clean code / add new text and style

--- src/index.js
+++ src/index.js
@@ -1,30 +1,48 @@
+// getting game elements
+const textLoseWin = document.getElementsByClassName("text-lose-win")[0];
+
 // getting player elements
 const playerBtnWrapper = document.querySelectorAll(".player-Btn-Wrapper")[0];
+const playerScoreText = document.getElementsByClassName("player-score-h1")[0];
 
 // getting pc elements
 const PcGameBtnWrapper = document.querySelectorAll(".Pc-game-Btn-Wrapper")[0];
+const pcScoreText = document.getElementsByClassName("Pc-score-h1")[0];
+
+// global element
+let pScore = 0;
+let pcScore = 0;
 
 // our game func
 function randNum(e) {
   // player functions
   const playerScore = e.target.id;
   playerBtnWrapper.children[playerScore].classList.add("active");
-  playerBtnWrapper.classList.add("active");
-  console.log("Player Point : ", playerScore);
+  //   playerBtnWrapper.classList.add("active");
 
   // pc functions
   const randNum = Math.round(Math.random() * 2);
-  console.log(PcGameBtnWrapper.children[randNum]);
   PcGameBtnWrapper.children[randNum].classList.add("active");
 
   // game logic
-  console.log("PC Point : ", randNum);
   if (playerScore > randNum) {
     console.log("payer won");
+    pScore = pScore + 1;
+    console.log(pScore);
+    playerScoreText.textContent = `YOU - ${pScore}`;
+    textLoseWin.textContent = `YOU WON`;
+    textLoseWin.style.color = "green";
   } else if (playerScore < randNum) {
     console.log("payer lost");
+    pcScore = pcScore + 1;
+    console.log(pcScore);
+    pcScoreText.textContent = `YOU - ${pcScore}`;
+    textLoseWin.textContent = `YOU LOST`;
+    textLoseWin.style.color = "red";
   } else {
     console.log("same");
+    textLoseWin.textContent = `DROW`;
+    textLoseWin.style.color = "gray";
   }
 }
 
